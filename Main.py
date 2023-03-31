cf107c4a625398bb59ef126d6a8a4455c172c100
lsolaez <110132473+lsolaez@users.noreply.github.com>
2023-03-31 17:03:07 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
-st.tittle(' Laboratorio 2 - Estructura de datos 2')
+st.title(' Laboratorio 2 - Estructura de datos 2')
 
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
